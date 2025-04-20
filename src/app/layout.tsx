39fa00e22f5c1281d78bc24e371efbe8da8ce78b
Guillaume G. <guillaumeag@users.noreply.github.com>
2025-04-20 17:11:40 +0200

    Setup Inter as default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+})
+
 export const metadata: Metadata = {
   title: "Todo Next",
   description: "Todo Next is a simple todo app built with Next.js",
