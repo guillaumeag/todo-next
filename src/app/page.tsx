a825bb1fbd1db13d2c71a3a3f615d26b29357449
Guillaume G. <guillaumeag@users.noreply.github.com>
2025-04-20 16:58:00 +0200

    Home: Update layout and add a title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main></main>
+    <main className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 max-w-2xl mx-auto">
+        <div className="text-center mb-8">
+          <h1 className="text-6xl font-black uppercase py-4">Todo Next</h1>
+          <p className="text-current/80">This is a todo list app using Next.js</p>
+        </div>
+      </div>
+    </main>
   );
 }
