be3d5322df412f38491f435fbb038a6986813ede
Guillaume G. <guillaumeag@users.noreply.github.com>
2025-04-20 18:25:31 +0200

    Setup an SQLite database with Drizzle

---
+++ src/db/schema.ts
@@ -0,0 +1,10 @@
+import { sql } from "drizzle-orm";
+import { integer, text, sqliteTable } from "drizzle-orm/sqlite-core";
+
+export const todo = sqliteTable("todos", {
+  id: integer("id", { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  title: text("title").notNull(),
+  completed: integer("completed", { mode: 'boolean' }).default(false).notNull(),
+  createdAt: integer("created_at", { mode: 'timestamp' }).default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+  updatedAt: integer("updated_at", { mode: 'timestamp' }).default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+});
