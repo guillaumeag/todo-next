0f8f068c03c8075ad9fcc7216493e7c56981e183
Guillaume G. <guillaumeag@users.noreply.github.com>
2025-04-21 17:37:31 +0200

    Fix todo list refresh after adding a new todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,19 @@ import Todos from "@/app/ui/todos";
 import Form from "@/app/ui/todo-form";
 import { getAll } from "@/actions/todoActions";
 
-// Get all todo items from the database
-const allTodos = await getAll();
+export default async function Home() {
+  // Get all todo items from the database
+  const allTodos = await getAll();
 
-// Transform the data to match the expected Promise type
-const transformedTodos = allTodos.map(todo => ({
-  id: todo.id,
-  title: todo.title,
-  completed: todo.completed,
-  created_at: todo.created_at,
-  update_at: todo.updated_at,
-}));
+  // Transform the data to match the expected Promise type
+  const transformedTodos = allTodos.map(todo => ({
+    id: todo.id,
+    title: todo.title,
+    completed: todo.completed,
+    created_at: todo.created_at,
+    update_at: todo.updated_at,
+  }));
 
-export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
       <div className="grid grid-cols-1 max-w-2xl mx-auto">
--- src/app/ui/todos.tsx
+++ src/app/ui/todos.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, FC } from 'react';
+import { FC } from 'react';
 import { todoType } from "@/types/todoType";
 
 interface Props {
@@ -8,13 +8,9 @@ interface Props {
 }
 
 const Todos: FC<Props> = ({ todos }) => {
-  
-  // State to manage the list of todo items
-  const [todoItems, setTodoItems] = useState<todoType[]>(todos);
-
   return (
     <ul>
-      {todoItems.map((item) => (
+      {todos.map((item) => (
         <li key={item.id} className="py-1">
           <span className="text-lg mr-2">{item.title}</span>
           <span className="text-sm text-gray-500">{item.completed ? "Completed" : "Not Completed"}</span>
